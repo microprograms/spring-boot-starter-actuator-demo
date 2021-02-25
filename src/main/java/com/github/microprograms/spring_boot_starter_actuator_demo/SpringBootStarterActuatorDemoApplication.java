f8e180ad995e9e01dc754433f7e914bbcd8240f0
xuzewei <microprograms@126.com>
2021-02-25 14:00:53 +0800

    actuator demo

---
+++ src/main/java/com/github/microprograms/spring_boot_starter_actuator_demo/SpringBootStarterActuatorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.microprograms.spring_boot_starter_actuator_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterActuatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterActuatorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/microprograms/spring_boot_starter_actuator_demo/SpringBootStarterActuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.microprograms.spring_boot_starter_actuator_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterActuatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
